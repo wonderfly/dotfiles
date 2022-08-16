3db1ace077319c2782c4e32c896a9eb2bbec33f3
Daniel Wang <wonderfly@waymo.com>
2022-08-16 16:58:25 -0700

    fish: fix which

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -87,15 +87,16 @@ end
 
 function which
   set -l target $argv[1]
-  set -l which_output (command which $target)
-  if string match -rq $target $which_output
-    echo $which_output
-    return
-  end
   set -l IFS ""
+  # Check if it's a function first.  Functions take precedence over commands.
   set -l functions_output (functions $target)
   if string match -rq $target $functions_output
     echo "$functions_output"
+    return
+  end
+  set -l which_output (command which $target)
+  if string match -rq $target $which_output
+    echo $which_output
     return
   end
   echo "`$target` is not a command, alias or function."
